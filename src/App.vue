a8900bb199e9c0b32361749c0845873c6a8d458f
muhonja <linahmuhonjaimbari@gmail.com>
2024-06-28 14:32:11 +0300

    add RouterView slot

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,26 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router'
+import { useCampaignStore } from "@/stores";
+
+const campaignStore = useCampaignStore()
+
 </script>
 
 <template>
-  <RouterView />
+<RouterView #default="{ Component, route }">
+  <template v-if="Component">
+    <component :is="Component" :key="route.fullPath" />
+
+  </template>
+  <template v-else-if="campaignStore.isAppFetching">
+    <div class="h-screen flex flex-col justify-center items-center">
+      <span class="loading loading-spinner loading-md"></span>
+      <p class="text-md text-neutral-500 dark:text-neutral-300">Loading...</p>
+    </div>
+
+
+  </template>
+</RouterView>
 </template>
 
 
