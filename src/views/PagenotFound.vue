2ab7224621f03d35b67ad5e46d13a057be9f3ba6
muhonja <linahmuhonjaimbari@gmail.com>
2024-07-01 12:57:46 +0300

    add PagenotFound component

---
+++ src/views/PagenotFound.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { useRouter  } from 'vue-router';
+
+const route = useRouter();
+const goBack = ()=>{
+  // window.history.back();
+  route.push('/')
+
+
+}
+</script>
+
+<template>
+  <div class="flex-1 flex flex-col justify-center items-center h-screen">
+    <img src="../assets/images/404-error.png" class="w-1/4">
+    <div class="text-center">
+      <h1 class="text-3xl  text-neutral-900 ">Page not Found</h1>
+      <p class="text-neutral-500">The page you are looking for is not available</p>
+    </div>
+    <div class="mt-4">
+      <button
+          @click="goBack"
+          class="btn btn-primary bt-sm lg:btn-lg md:btn-md"
+      >Go back</button>
+    </div>
+  </div>
+</template>
